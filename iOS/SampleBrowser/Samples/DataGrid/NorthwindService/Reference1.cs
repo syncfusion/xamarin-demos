f1482dc68a3833efeb24778a915216f63a3425cd
ROOT\testing <buildautomation@syncfusion.com>
2019-04-01 18:25:01 +0530

    v17.1.0.38

--- iOS/SampleBrowser/Samples/DataGrid/NorthwindService/Reference1.cs
+++
@@ -1,7 +0,0 @@
-#region Copyright Syncfusion Inc. 2001-2018.
-// Copyright Syncfusion Inc. 2001-2018. All rights reserved.
-// Use of this code is subject to the terms of our license.
-// A copy of the current license can be obtained at any time by e-mailing
-// licensing@syncfusion.com. Any infringement will be prosecuted under
-// applicable laws. 
-#endregion
