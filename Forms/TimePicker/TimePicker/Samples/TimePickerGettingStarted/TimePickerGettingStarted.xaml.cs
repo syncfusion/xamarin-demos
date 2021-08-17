c9b534fe468901684ad279665f2bf41fa20a9e18
SyncfusionInstall <buildautomation@syncfusion.com>
2021-08-17 17:33:29 +0530

    v19.2.0.55

---
+++ Forms/TimePicker/TimePicker/Samples/TimePickerGettingStarted/TimePickerGettingStarted.xaml.cs
@@ -0,0 +1,58 @@
+#region Copyright Syncfusion Inc. 2001-2021.
+// Copyright Syncfusion Inc. 2001-2021. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using SampleBrowser.Core;
+using Syncfusion.ListView.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SampleBrowser.SfTimePicker
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TimePickerGettingStarted : SampleView
+    {
+        /// <summary>
+        /// Time Picker GettingStarted
+        /// </summary>
+        public TimePickerGettingStarted()
+        {
+            InitializeComponent();
+            if (Device.RuntimePlatform == Device.UWP)
+            {
+                layoutRootGrid.WidthRequest = 500;
+            }
+        }
+
+        /// <summary>
+        /// ListView Swipe Ended event
+        /// </summary>
+        /// <param name="sender">sender value</param>
+        /// <param name="e">Event args</param>
+        private void ListView_SwipeEnded(object sender, Syncfusion.ListView.XForms.SwipeEndedEventArgs e)
+        {
+            if (e.SwipeDirection == Syncfusion.ListView.XForms.SwipeDirection.Right)
+            {
+                e.SwipeOffset = sfListView.Width;
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Task.Delay(500);
+                    viewModel.Alarms.RemoveAt(e.ItemIndex);
+                });
+            }
+        }
+    }
+}
+
---
+++ iOS/SampleBrowser/Samples/Calendar/README.md
@@ -0,0 +1,10 @@
+## SfCalendar
+Calendar is a user interface component that displays Gregorian calendar. This component allows the user to select a date. It also provides a gesture-friendly UI to perform operations such as navigations, events, and more. 
+
+| Sample | Description |
+|-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|--------------------------------------------------------------------------------------------------------------------------|
+| GettingStarted | Showcases different view modes support: month and year views. |
+| Blackout Dates | Showcases the specific date blackout capability of calendar. |
+| Inline Events | Showcases the inline events in inline view mode and agenda view mode in a specific date of the month. |
+| Localization | Showcases localization and globalization. |
+| Customization | Showcases the month cell customization using custom view with restriction support to navigate beyond the specified minimum and maximum dates. |
